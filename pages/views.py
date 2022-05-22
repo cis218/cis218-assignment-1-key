53774a38feeb4aaac29887c4dff00c2d7a0233cf
David Barnes <dbarnes@kvcc.edu>
2022-05-22 14:43:03 -0400

    Add base template contents and placeholders for each individual page. Wire up each page to work.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    """Show Home Page"""
+
+    template_name = "home.html"
+
+
+class ProjectsPageView(TemplateView):
+    """Show Projects Page"""
+
+    template_name = "projects.html"
+
+
+class ContactPageView(TemplateView):
+    """Show Contact Page"""
+
+    template_name = "contact.html"
